0e78031c487c66c7be5b5cb4e5bf119247eb19ef
mike <fixitman.mike@gmail.com>
2025-07-26 00:40:13 -0400

    changed minimap to pass data in ImageTexture

--- scripts/minimap.gd
+++ scripts/minimap.gd
@@ -12,15 +12,21 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta):
 	targets.clear()
-	for target in get_tree().get_nodes_in_group("targets"):
-		
+	for target in get_tree().get_nodes_in_group("targets"):		
 		targets.append(player.to_local(target.global_position))
-		
-		
-	for i in range(0,20 - targets.size()-1):
-		targets[i] = Vector3.ZERO
 	
-	$CanvasLayer/TextureRect.material.set_shader_parameter("target_data",targets)
+	
+		
+	var image = Image.create(1,500,false,Image.FORMAT_RGBAF)
+	image.set_pixel(0,0,Color((float(targets.size())),0,0))
+	for i in range(targets.size()):
+		image.set_pixel(0,i+1,Color(targets[i].x,targets[i].y,targets[i].z))
+		
+		
+		
+	var tex = ImageTexture.create_from_image(image)
+	
+	$CanvasLayer/TextureRect.material.set_shader_parameter("target_data",tex)
 	
 	
 	
